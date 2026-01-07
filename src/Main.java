89613b9105197daefb2039dc4d3809bfc1fdfa94
Cameron Stone <226145444+Cameron-Stone-acs@users.noreply.github.com>
2026-01-07 09:42:30 -0800

    code

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        String[][] language = new  String[][]
+                { // {language name in english, hello world in that language}
+                        {"English", "Hello World"}, {"Abkhaz", "Салам Адунеи"},
+                        {"Acehnese", "Halo Donya"}, {"Acholi", "Apwoyo lobo"},
+                        {"Afar", "Nagaale Baadal"}, {"Afrikaans", "Hallo Wêreld"},
+                        {"Albanian", "Përshëndetje Botë"}, {"Alur", "Amothi ngom"},
+                        {"Amharic", "ሰላም አለም"}, {"Arabic", "مرحبا بالعالم"},
+                        {"Armenian", "Բարև աշխարհ"}, {"Assamese", "হেল্ল' ৱৰ্ল্ড"},
+                        {"Avar", "Салам Дунял"}, {"Awadhi", "हैलो वर्ल्ड"},
+                        {"Aymara", "Suma Uraqpachan"}, {"Azerbaijani", "Salam Dünya"},
+                        {"Balinese", "Halo Jagat"}, {"Baluchi", "سلام دنیا"},
+                        {"Bambara", "Bonjour Monde"}, {"Baoulé", "Bonjour Monde"},
+                        {"Bashkir", "Һаумыһығыҙ Донъя"}, {"Basque", "Kaixo Mundua"},
+                        {"Batak Karo", "Halo Dunia"}, {"Batak Simalungun", "Horas Dunia"},
+                        {"Batak Toba", "Horas Dunia"}, {"Belarusian", "Прывітанне, свет"},
+                        {"Bemba", "Mwapoleni Icalo conse"}, {"Bengali", "হ্যালো ওয়ার্ল্ড"},
+                        {"Betawi", "Halo Dunia"}, {"Bhojpuri", "नमस्कार दुनिया के बा"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"Despacito", "Despacito"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+                        {"English", "Hello World"}, {"English", "Hello World"},
+
+
+
+                };
+    }
+}
